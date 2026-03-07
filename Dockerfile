a886706f1a06619d9d74ab6186bb3a53073a48ba
Hangga Aji Sayekti <bazeniancode@gmail.com>
2026-03-07 12:55:03 +0700

    unused import

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # =========================
 # Stage 1: Build
 # =========================
-FROM eclipse-temurin:17-jdk AS builder
+FROM eclipse-temurin:17-jre-jammy AS builder
 
 WORKDIR /app
 
--- src/main/kotlin/id/web/hangga/Application.kt
+++ src/main/kotlin/id/web/hangga/Application.kt
@@ -8,15 +8,14 @@ import id.web.hangga.routes.PriceRoutes
 import id.web.hangga.service.ExternalCryptoService
 import id.web.hangga.service.FallbackCache
 import id.web.hangga.stream.PriceFlow
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.slf4j.LoggerFactory
 
 fun main() {
     // Force init serialization
